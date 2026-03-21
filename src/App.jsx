cb722798f4f76af99383c196c0094b5d33077ea6
sv.189 <saleanv.17@gmail.com>
2026-03-20 22:03:40 -0700

    Initial React project setup with Vite and React Router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+       
